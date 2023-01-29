55e565fcdabee378fb4c1765ba6ad0fd4d89a87b
Cainã Pimenta <caina.gomes@alfaerp.com.br>
2023-01-29 12:09:24 -0300

    Criando as primeiras entidades (Entity)

---
+++ src/API.Domain/Entities/BaseEntity.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API.Domain.Entities
+{
+    public abstract class BaseEntity
+    {
+        private DateTime? _createAt;
+
+        [Key]
+        public Guid Id { get; set; }
+        public DateTime? CreateAt
+        {
+            get { return _createAt; }
+            set { _createAt = (value == null ? DateTime.UtcNow : value); }
+        }
+        public DateTime? UpdateAt { get; set; }
+
+        public BaseEntity()
+        {
+
+        }
+    }
+}
---
+++ src/API.Domain/Entities/UserEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API.Domain.Entities
+{
+    public class UserEntity : BaseEntity
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        public UserEntity()
+        {
+
+        }
+    }
+}
