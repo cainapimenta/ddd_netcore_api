47b9c533fc22277200951aa49a77d73372aa91b3
Cainã Pimenta <caina.pimenta@xnetweb.com.br>
2023-07-16 23:28:29 -0300

    57. Api.Data - Criar ContextFactory

---
+++ src/API.Data/Context/ContextFactory.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace API.Data.Context
+{
+    public class ContextFactory : IDesignTimeDbContextFactory<MyContext>
+    {
+        /// <summary>
+        /// Usado para criar migrações
+        /// </summary>
+        /// <param name="args"></param>
+        /// <returns></returns>
+        public MyContext CreateDbContext(string[] args)
+        {
+            var connectionString = "Server=localhost;Port=3306;Database=dbAPI;Uid=root;Pwd=B1admin@";
+            var optionBuilder = new DbContextOptionsBuilder<MyContext>();
+            optionBuilder.UseMySql(connectionString);
+
+            return new MyContext(optionBuilder.Options);
+        }
+    }
+}
