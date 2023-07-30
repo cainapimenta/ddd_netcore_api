a79f07234e456d8ca0efa53be516045fb8f76d30
Cainã Pimenta <caina.pimenta@xnetweb.com.br>
2023-07-30 17:06:08 -0300

    66. Api.Domain - Implementar Interface UserService

---
+++ src/API.Domain/Interfaces/Services/User/IUserService.cs
@@ -0,0 +1,17 @@
+﻿using API.Domain.Entities;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace API.Domain.Interfaces.Services.User
+{
+    public interface IUserService
+    {
+        Task<UserEntity> Get(Guid id);
+        Task<IEnumerable<UserEntity>> GetAll();
+        Task<UserEntity> Post(UserEntity entity);
+        Task<UserEntity> Put(UserEntity entity);
+        Task<bool> Delete(Guid id);
+    }
+}
