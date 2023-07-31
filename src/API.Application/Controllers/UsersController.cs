42a4ee029ed92d0757bc72f9a2cd84024e7a7c0c
Cainã Pimenta <caina.pimenta@xnetweb.com.br>
2023-07-30 22:27:35 -0300

    69. Api.Application - Adicionar Referencias e Criar a classe UsersController

---
+++ src/API.Application/Controllers/UsersController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API.Application.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
