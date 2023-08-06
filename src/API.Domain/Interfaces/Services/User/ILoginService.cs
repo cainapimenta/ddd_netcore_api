31b98170f6945a4593202545338044d02d8b04b2
Cainã Pimenta <caina.pimenta@xnetweb.com.br>
2023-08-05 22:51:32 -0300

    81. Api.Domain - ILoginService

---
+++ src/API.Domain/Interfaces/Services/User/ILoginService.cs
@@ -0,0 +1,13 @@
+﻿using API.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API.Domain.Interfaces.Services.User
+{
+    public interface ILoginService
+    {
+        Task<object> FindByLogin(UserEntity user);
+    }
+}
