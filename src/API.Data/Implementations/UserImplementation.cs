3e9978b756172bd8767f756c2a34f109cafad143
Cainã Pimenta <caina.pimenta@xnetweb.com.br>
2023-08-05 22:49:23 -0300

    80. Api.Data - UserImplementation

---
+++ src/API.Data/Implementations/UserImplementation.cs
@@ -0,0 +1,27 @@
+﻿using API.Data.Context;
+using API.Data.Repository;
+using API.Domain.Entities;
+using API.Domain.Repository;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API.Data.Implementations
+{
+    public class UserImplementation : BaseRepository<UserEntity>, IUserRepository
+    {
+        private DbSet<UserEntity> _dbUsers;
+
+        public UserImplementation(MyContext context) : base(context)
+        {
+            _dbUsers = context.Set<UserEntity>();
+        }
+
+        public async Task<UserEntity> FindByLogin(string email)
+        {
+            return await _dbUsers.FirstOrDefaultAsync(x => x.Email == email);
+        }
+    }
+}
