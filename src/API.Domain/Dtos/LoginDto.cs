8b4a30b6465a42542368ae477746148b33b15c76
Cainã Pimenta <caina.pimenta@xnetweb.com.br>
2023-08-06 12:27:53 -0300

    85. Api.Domain - Criar Login Dto (Data Transfer Object)

---
+++ src/API.Domain/Dtos/LoginDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.Domain.Dtos
+{
+    public class LoginDto
+    {
+        [Required(ErrorMessage = "Email é um campo obrigatório para o Login.")]
+        [EmailAddress(ErrorMessage = "E-mail em formato inválido.")]
+        [StringLength(100, ErrorMessage = "E-mail deve ter no máximo {1} caracateres.")]
+        public string Email { get; set; }
+    }
+}
