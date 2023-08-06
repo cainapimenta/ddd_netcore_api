cd75869ec4eeff956d715f52c905266f1d93822b
Cainã Pimenta <caina.pimenta@xnetweb.com.br>
2023-08-05 23:06:15 -0300

    83. Api.Application - LoginController

---
+++ src/API.Application/Controllers/LoginController.cs
@@ -0,0 +1,49 @@
+﻿using API.Domain.Entities;
+using API.Domain.Interfaces.Services.User;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace API.Application.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private readonly ILoginService _service;
+
+        public LoginController(ILoginService service)
+        {
+            _service = service;
+        }
+
+        [HttpPost]
+        public async Task<object> Login([FromBody] UserEntity user)
+        {
+            if(!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            if (user == null)
+                return BadRequest();
+
+            try
+            {
+                var result = await _service.FindByLogin(user);
+
+                if (result == null)
+                    return NotFound();
+                else
+                    return Ok(result);
+            }
+            catch (ArgumentException ex)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError, ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError, ex.Message);
+            }
+        }
+    }
+}
